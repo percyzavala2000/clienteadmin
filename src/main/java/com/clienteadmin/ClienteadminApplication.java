074b800d35487e4515dcbcb83997ac7ebbfc63bf
percyzavala <percyzavala@gmail.com>
2025-04-26 15:42:56 -0500

    Inicio de Proyecto ClienteAdmin

---
+++ src/main/java/com/clienteadmin/ClienteadminApplication.java
@@ -0,0 +1,13 @@
+package com.clienteadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clienteadmin/ClienteadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clienteadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
