d7d3ec59e91a844fb3a2f6444c5fef4268546c8b
percyzavala <percyzavala@gmail.com>
2025-04-27 12:43:09 -0500

    se agrego implementacion de agregar cliente

--- src/main/java/com/clienteadmin/ClienteadminApplication.java
+++ src/main/java/com/clienteadmin/ClienteadminApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClienteadminApplication {
 
 	public static void main(String[] args) {
+
+    
 		SpringApplication.run(ClienteadminApplication.class, args);
 	}
 
